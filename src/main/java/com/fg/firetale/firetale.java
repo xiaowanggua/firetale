0f0cadf298aa704ebde49c27dbcf71af66046f7b
xiaowanggua <2362301355@qq.com>
2022-05-09 18:27:50 +0800

    init

---
+++ src/main/java/com/fg/firetale/firetale.java
@@ -0,0 +1,25 @@
+package com.fg.firetale;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(firetale.MODID)
+public class firetale {
+    public static final String MODID="firetale";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+}
