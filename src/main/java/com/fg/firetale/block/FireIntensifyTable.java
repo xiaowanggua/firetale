3a926a6dde8c9beb7953a34e3f2e0bfa93345c6b
xiaowanggua <2362301355@qq.com>
2022-05-13 18:09:45 +0800

    fitGUi WIP2

--- src/main/java/com/fg/firetale/block/FireIntensifyTable.java
+++ src/main/java/com/fg/firetale/block/FireIntensifyTable.java
@@ -8,7 +8,6 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
